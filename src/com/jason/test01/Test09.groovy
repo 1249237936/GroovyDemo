3a204e9ac265a503bbbf786e5f4c04055f36e4af
Jason <15521215849@163.com>
2022-01-31 14:43:45 +0800

    closure String

--- src/com/jason/test01/Test09.groovy
+++ src/com/jason/test01/Test09.groovy
@@ -2,3 +2,15 @@ package com.jason.test01
 
 def s = "hi groovy 2023"
 println s.each{ println it}
+
+println s.find {it.isNumber()}
+
+def list = s.findAll {it.isNumber()}
+println list.toListString()
+
+println s.any {it.isNumber()}
+
+println s.every {it.isNumber()}
+
+def list2 = s.collect {it.toUpperCase()}
+println list2.toListString()
